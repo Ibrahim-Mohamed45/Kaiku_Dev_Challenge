8a97f03a9cb482b285b40b971d0b50039dce2e1b
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-06-17 15:16:04 +0100

    Add more info modal

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,28 @@ import startupsData from '../kaiku_data.json';
 const App = () => {
   const [startups, setStartups] = useState(startupsData);
   const [acceptedStartups, setAcceptedStartups] = useState([]);
+  const [selectedStartup, setSelectedStartup] = useState(null);
 
-  const acceptStartup = (id) => {
-    const startup = startups.find((startup) => startup.id === id);
-    setAcceptedStartups((prevAcceptedStartups) => [...prevAcceptedStartups, startup]);
-    setStartups((prevStartups) => prevStartups.filter((startup) => startup.id !== id));
+  const openModal = (startup) => {
+    setSelectedStartup(startup);
   };
 
-  const discardStartup = (id) => {
-    setStartups((prevStartups) => prevStartups.filter((startup) => startup.id !== id));
+  const closeModal = () => {
+    setSelectedStartup(null);
+  };
+
+  const acceptStartup = (startupId) => {
+    const selectedStartup = startups.find((startup) => startup.id === startupId);
+    if (selectedStartup) {
+      setStartups((prevStartups) => prevStartups.filter((startup) => startup.id !== startupId));
+      setAcceptedStartups((prevAcceptedStartups) => [...prevAcceptedStartups, selectedStartup]);
+    }
+    closeModal();
+  };
+
+  const discardStartup = (startupId) => {
+    setStartups((prevStartups) => prevStartups.filter((startup) => startup.id !== startupId));
+    closeModal();
   };
 
   return (
@@ -27,6 +40,7 @@ const App = () => {
             <p>{startup.oneLiner}</p>
             <button onClick={() => acceptStartup(startup.id)}>Accept</button>
             <button onClick={() => discardStartup(startup.id)}>Discard</button>
+            <button onClick={() => openModal(startup)}>More Info</button>
           </li>
         ))}
       </ul>
@@ -37,9 +51,36 @@ const App = () => {
           <li key={startup.id}>
             <h3>{startup.name}</h3>
             <p>{startup.oneLiner}</p>
+            <button onClick={() => discardStartup(startup.id)}>Discard</button>
+            <button onClick={() => openModal(startup)}>More Info</button>
           </li>
         ))}
       </ul>
+
+      {selectedStartup && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <h2>{selectedStartup.name}</h2>
+            <p>Industry: {selectedStartup.industry}</p>
+            <p>Stage: {selectedStartup.stage}</p>
+            {/* Add more details as needed */}
+            <div>
+              {startups.some((startup) => startup.id === selectedStartup.id) && (
+                <>
+                  <button onClick={() => acceptStartup(selectedStartup.id)}>Accept</button>
+                  <button onClick={() => discardStartup(selectedStartup.id)}>Discard</button>
+                </>
+              )}
+              {acceptedStartups.some((startup) => startup.id === selectedStartup.id) && (
+                <button onClick={() => discardStartup(selectedStartup.id)}>Discard</button>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
