7adba2f958c694e83142e5582ce57b481c75d6aa
Ibrahim Mohamed <ibrahim.mohamed1218@gmail.com>
2023-06-17 14:33:19 +0100

    Add accept & discard functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import startupsData from '../kaiku_data.json';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [startups, setStartups] = useState(startupsData);
+  const [acceptedStartups, setAcceptedStartups] = useState([]);
+
+  const acceptStartup = (id) => {
+    const startup = startups.find((startup) => startup.id === id);
+    setAcceptedStartups((prevAcceptedStartups) => [...prevAcceptedStartups, startup]);
+    setStartups((prevStartups) => prevStartups.filter((startup) => startup.id !== id));
+  };
+
+  const discardStartup = (id) => {
+    setStartups((prevStartups) => prevStartups.filter((startup) => startup.id !== id));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Startup Signups</h1>
+      <h2>Incoming Startups</h2>
+      <ul>
+        {startups.map((startup) => (
+          <li key={startup.id}>
+            <h3>{startup.name}</h3>
+            <p>{startup.oneLiner}</p>
+            <button onClick={() => acceptStartup(startup.id)}>Accept</button>
+            <button onClick={() => discardStartup(startup.id)}>Discard</button>
+          </li>
+        ))}
+      </ul>
 
+      <h2>Accepted Startups</h2>
+      <ul>
+        {acceptedStartups.map((startup) => (
+          <li key={startup.id}>
+            <h3>{startup.name}</h3>
+            <p>{startup.oneLiner}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 export default App
+
